5567132348e7a65184fec243b8673e1719aa03a1
albertob <alberto.bonora@gmail.com>
2018-09-18 23:18:34 -0400

    wired up sseasons

---
+++ src/components/seasons.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="seasons-wrapper">
+        <h1>Seasons</h1>
+        <ul>
+            <li v-for="season in seasons">
+                <h1>{{season.Season}}</h1>
+                <span>Winner: {{ season.Winner.Team }}</span>
+                <span>Runner Up{{ season.RunnerUp.Team }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    name: 'Seasons',
+    data() {
+        return {
+            seasons: [],
+            errors: []
+        }
+    },
+    //fetch posts
+    created(){
+        axios.get('http://rssl.albertobonora.ca/api/rssl-app.json').then(response => {
+            this.seasons = response.data.History
+        })
+        .catch( e=> {
+            this.errors.push(e)
+        })
+    }
+};
+</script>
+<style scoped lang="scss">
+ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    li{
+        padding: 0;
+        margin: 0;
+        span{
+            display: block;
+        }
+    }
+}
+</style>
---
+++ src/views/History.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="history">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <seasons/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import seasons from '@/components/seasons.vue';
+
+export default {
+  name: 'history',
+  components: {
+    seasons,
+  },
+};
+</script>
